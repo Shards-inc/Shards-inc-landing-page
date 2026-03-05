e1adacf28c0ddc4ade0a8f61dc63ead1d1cf8dcf
Ayai <ayais12210@gmail.com>
2026-03-05 02:47:28 +0000

    Create assistant.tsx

---
+++ client/src/components/ui/assistant.tsx
@@ -0,0 +1,12 @@
+<script>
+  (function() {
+    const script = document.createElement('script');
+    script.src = 'https://assistloop.ai/assistloop-widget.js';
+    script.onload = function() {
+      AssistLoopWidget.init({
+        agentId: "8e955b37-7b3d-47c3-9bd9-291fec4b5fa0" 
+      });
+    };
+    document.head.appendChild(script);
+  })();
+</script>
